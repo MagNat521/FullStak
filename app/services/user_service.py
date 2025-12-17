18c2d300feb3d52ae1513b20708d831ef3ab4ecb
MagNat521 <81864811+MagNat521@users.noreply.github.com>
2025-12-18 02:35:02 +0300

    Create user_service.py
    Сервис пользователя

---
+++ app/services/user_service.py
@@ -0,0 +1,30 @@
+# app/services/user_service.py
+from sqlalchemy.orm import Session
+
+from app import models
+from app.core.security import get_password_hash, verify_password
+from app.schemas.user import UserCreate
+
+
+def get_by_email(db: Session, email: str) -> models.User | None:
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, data: UserCreate) -> models.User:
+    user = models.User(
+        email=data.email,
+        hashed_password=get_password_hash(data.password),
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def authenticate(db: Session, email: str, password: str) -> models.User | None:
+    user = get_by_email(db, email)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
